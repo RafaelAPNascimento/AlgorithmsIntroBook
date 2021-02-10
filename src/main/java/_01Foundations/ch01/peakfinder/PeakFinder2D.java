487d5884029ac6b55da4ad9e555dc60a04afb3fd
RafaelAPNascimento <rfx.go.on@gmail.com>
2021-02-10 11:04:59 -0300

    dev 2d peak

---
+++ src/main/java/_01Foundations/ch01/peakfinder/PeakFinder2D.java
@@ -0,0 +1,9 @@
+package _01Foundations.ch01.peakfinder;
+
+/**
+ * Given a 2D array of integer, a peak is whatever element whose value is greater or
+ * equal to its neighbours. A peak in a 2D array can have at most 4 neighbours.
+ */
+public class PeakFinder2D {
+
+}
