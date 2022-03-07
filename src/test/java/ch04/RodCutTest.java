b0eddea55684a192e2d9486003fbf0eae3ce4364
RafaelAPNascimento <rfx.go.on@gmail.com>
2022-03-07 10:30:24 -0300

    rod cut timming

--- src/test/java/ch04/RodCutTest.java
+++ src/test/java/ch04/RodCutTest.java
@@ -6,11 +6,13 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.time.Instant;
+import java.util.logging.Logger;
+
 public class RodCutTest {
 
     private int[] prices = {1, 5, 8, 9, 10, 17, 17, 20};
 
-    @Disabled
     @DisplayName("Should return optimun by length 4, using recursive approach")
     @Test
     public void shouldPassByLength4() {
@@ -20,7 +22,7 @@ public class RodCutTest {
         Assertions.assertEquals(10, opt);
     }
 
-    @Disabled
+
     @DisplayName("Should return optimun by length 8, using recursive approach")
     @Test
     public void shouldPassByLength8() {
@@ -47,4 +49,32 @@ public class RodCutTest {
         int opt = rodCutting.dpOptimun(prices, 8);
         Assertions.assertEquals(22, opt);
     }
+
+    @DisplayName("compare execution time between recursive and memoized approach")
+    @Test
+    public void compareBothApproaches() {
+
+        RodCutting rodCutting = new RodCutting();
+
+        long start = Instant.now().toEpochMilli();
+        int opt1 = rodCutting.optimun(prices, 8);
+        long end = Instant.now().toEpochMilli();
+
+        long timeRecursiveApproach = end - start;
+
+        start = Instant.now().toEpochMilli();
+        int opt2 = rodCutting.dpOptimun(prices, 8);
+        end = Instant.now().toEpochMilli();
+
+        long timeMemoizedApproach = end - start;
+
+        LOG.info("Recursive approach " + timeRecursiveApproach);
+        LOG.info("DP approach " + timeMemoizedApproach);
+
+        Assertions.assertEquals(opt1, opt2);
+        Assertions.assertEquals(22, opt1);
+        Assertions.assertTrue(timeMemoizedApproach <= timeRecursiveApproach, "Memoized approach was not more efficient than recursive");
+    }
+
+    Logger LOG = Logger.getLogger(RodCutTest.class.getName());
 }
